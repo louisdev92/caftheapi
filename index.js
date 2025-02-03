8220dce2c2688830c70a6913c9909bf0aa6cbd69
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-03 12:21:13 +0100

    Commit

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require("express");
 /* npm install cors */
 const cors = require("cors");
 /* npm install dotenv */
+const db = require("./db"); // Connexion à MySQL
+const routes = require("./endpoints"); // Les routes de l'API
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Utilisation des routes
+app.use("/api", routes);

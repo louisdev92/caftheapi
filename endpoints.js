91477bd52165687cab663f3921c11d1ae0f02abc
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-03 15:22:45 +0100

    Commit endpoints produit id

--- endpoints.js
+++ endpoints.js
@@ -15,4 +15,24 @@ router.get("/produits", (req, res) => {
     });
 });
 
+/*
+ * Route : Récupérer un produit par son ID
+ * Get /api/produits/:id
+ * Exemple : GET /api/produits/3
+ */
+
+router.get("/produits/:id", (req, res) => {
+    const {id} = req.params; // const id = req.params.id;
+
+    db.query("SELECT * FROM produits WHERE id = ?", [id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: "Erreur du serveur" });
+        }
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Produit non trouvé" });
+        }
+        res.json(result[0]); // Retournera que Le premier résultat
+    });
+});
+
 module.exports = router;
